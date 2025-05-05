aa8d62c2109bd1b6a41088a67c3c935fb3bf5496
vgotu99 <vgotu99@gmail.com>
2025-05-05 23:10:47 +0900

    feat: add better error handling to response processing

--- src/utils/response/processResponse.ts
+++ src/utils/response/processResponse.ts
@@ -1,14 +1,21 @@
+import { ERROR_CODES, NextFetchError } from "../../errors";
 import type { NextFetchResponse } from "../../types";
 
-
 export const processResponse = async <T>(
-  response: Response,
+  response: Response
 ): Promise<NextFetchResponse<T>> => {
-  const NextFetchResponse = response as NextFetchResponse<T>;
+  const nextFetchResponse = response as NextFetchResponse<T>;
 
-  if (response.headers.get('content-type')?.includes('application/json')) {
-    NextFetchResponse.data = await response.json();
+  if (response.headers.get("content-type")?.includes("application/json")) {
+    try {
+      nextFetchResponse.data = await response.json();
+    } catch (error) {
+      throw new NextFetchError("Invalid JSON response", {
+        request: new Request(response.url),
+        code: ERROR_CODES.ERR_BAD_RESPONSE,
+      });
+    }
   }
 
-  return NextFetchResponse;
+  return nextFetchResponse;
 };
