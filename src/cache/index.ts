f953c5bf0fc61ab8f017d2e76c250f5fc7d2091b
vgotu99 <vgotu99@gmail.com>
2025-07-09 19:47:48 +0900

    feat: barrel export cache optimization and config apis

--- src/cache/index.ts
+++ src/cache/index.ts
@@ -3,3 +3,11 @@ export { clientCacheManager } from "./ClientCacheManager";
 export { revalidationDetector } from "./RevalidationDetector";
 export { syncManager } from "./SyncManager";
 export { revalidateClientTag } from "./revalidateClientTag";
+export { cacheOptimizer } from "./CacheOptimizer";
+export {
+  configureCacheSettings,
+  getCacheMetrics,
+  triggerCacheCleanup,
+  checkStorageQuota,
+  getDefaultCacheConfig,
+} from "./config";
