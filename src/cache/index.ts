110fec386aa17cef8594dbd9b35cff3b832ca47d
vgotu99 <vgotu99@gmail.com>
2025-07-09 18:17:35 +0900

    feat: implement client cache revalidation method by tags

--- src/cache/index.ts
+++ src/cache/index.ts
@@ -2,3 +2,4 @@ export { getRequestCache } from "./requestCache";
 export { clientCacheManager } from "./ClientCacheManager";
 export { revalidationDetector } from "./RevalidationDetector";
 export { syncManager } from "./SyncManager";
+export { revalidateClientTag } from "./revalidateClientTag";
