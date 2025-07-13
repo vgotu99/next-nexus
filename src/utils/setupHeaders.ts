61ff73166fc199c2232d49b8fea672d5bf0839a4
vgotu99 <vgotu99@gmail.com>
2025-07-13 18:18:51 +0900

    feat: enhance setupHeaders util to support Headers object and array formats

--- src/utils/setupHeaders.ts
+++ src/utils/setupHeaders.ts
@@ -1,12 +1,22 @@
 export const setupHeaders = (
   defaultHeaders: HeadersInit = {},
-  configHeaders: HeadersInit = {},
+  configHeaders: HeadersInit = {}
 ) => {
   const headers = new Headers(defaultHeaders);
 
-  Object.entries(configHeaders).forEach(([key, value]) => {
-    if (value) headers.set(key, value);
-  });
+  if (configHeaders instanceof Headers) {
+    configHeaders.forEach((value, key) => {
+      if (value) headers.set(key, value);
+    });
+  } else if (Array.isArray(configHeaders)) {
+    configHeaders.forEach(([key, value]) => {
+      if (value) headers.set(key, value);
+    });
+  } else if (configHeaders && typeof configHeaders === 'object') {
+    Object.entries(configHeaders).forEach(([key, value]) => {
+      if (value) headers.set(key, value);
+    });
+  }
 
   return headers;
 };
