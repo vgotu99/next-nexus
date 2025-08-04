8687be83261a18777f5b83258ee5d05b185ed0cd
vgotu99 <vgotu99@gmail.com>
2025-08-04 22:05:16 +0900

    feat: define headers constants for optimizing server-client data fetching using client cache

--- src/constants/cache.ts
+++ src/constants/cache.ts
@@ -1,2 +1,10 @@
 export const DEFAULT_CLIENT_CACHE_MAX_SIZE = 200;
 export const MAX_CACHE_KEY_LENGTH = 1000;
+
+export const HEADERS = {
+  CLIENT_CACHE: 'x-next-fetch-client-cache',
+  REQUEST_ETAG: 'x-next-fetch-request-etag',
+  RESPONSE_ETAG: 'x-next-fetch-response-etag',
+  CACHE_REVALIDATION: 'x-next-fetch-cache-revalidation',
+  CACHE_STATUS: 'x-next-fetch-cache-status',
+} as const;
