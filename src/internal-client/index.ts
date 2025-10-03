47442ffffe115faf1667213675f3019049282070
vgotu99 <vgotu99@gmail.com>
2025-10-04 02:29:22 +0900

    feat: isolate client components to improve chunk splitting

---
+++ src/internal-client/index.ts
@@ -0,0 +1,9 @@
+'use client';
+
+export { NexusHydrationDispatcher } from '@/client/NexusHydrationDispatcher';
+export { NexusHydrator } from '@/client/NexusHydrator';
+export { NexusRscInitializer } from '@/core/NexusRscInitializer';
+export {
+  SingleDefinitionClientRenderer,
+  GroupDefinitionsClientRenderer,
+} from '@/components/ClientRenderer';
